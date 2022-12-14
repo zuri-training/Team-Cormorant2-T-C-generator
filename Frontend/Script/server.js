dea427e061694c20a66b70a3b92a760835d696d8
Abike Ladele <71263235+laabike@users.noreply.github.com>
2022-12-14 04:50:08 +0100

    Updated work (#26)
    Co-authored-by: unknown <Abike Ladele>

--- Frontend/Script/server.js
+++ Frontend/Script/server.js
@@ -1,4 +1,4 @@
-const API = "http://localhost:5000/api/v1";
+const API = "http://localhost:5000/api";
 
 async function fetchAPI(data, endpoint, method) {
     try {
@@ -10,8 +10,8 @@ async function fetchAPI(data, endpoint, method) {
             body: JSON.stringify(data), //converting the object to a JSON file
         });
 
-        const result = await response.json(); //conerting the response back to object
-        console.log(result);
+        const result = await response.json(); //converting the response back to object
+        // console.log(result);
         return result;
     } 
     catch (error) {
